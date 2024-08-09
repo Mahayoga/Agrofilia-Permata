55d790862c8595ea172bc250c6f23adc9ed899e4
Mahayoga <myoga.bahtiar@gmail.com>
2024-08-09 20:26:55 +0700

    Nambah item sidebar Data Master

--- resources/views/dashboard-template.blade.php
+++ resources/views/dashboard-template.blade.php
@@ -69,6 +69,14 @@
           </a>
         </li>
         <li class="nav-item">
+          <a class="nav-link text-white " href="{{ route('historyDataView') }}">
+            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="material-icons opacity-10">table_view</i>
+            </div>
+            <span class="nav-link-text ms-1">Riwayat Data</span>
+          </a>
+        </li>
+        <li class="nav-item">
           <a class="nav-link text-white " href="../pages/notifications.html">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
               <i class="material-icons opacity-10">notifications</i>
