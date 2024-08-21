f7dbb96dc76f1d16264131b6767a7b2b821cbfa1
Mahayoga <myoga.bahtiar@gmail.com>
2024-08-22 01:53:58 +0700

    Nambah function buat baca sensor suhu, udara, cahaya, dan tanah per minggu nya

--- app/Http/Controllers/SensorController.php
+++ app/Http/Controllers/SensorController.php
@@ -37,44 +37,126 @@ class SensorController extends Controller {
         ]);
     }
 
-    public function kebunOptions() {
-        $daftarKebun = KebunModel::select()->get();
-        return response()->json([
-            "daftar_kebun" => $daftarKebun
-        ]);
-    }
-
-    public function testQuery() {
-        /* 
-            SELECT *, COUNT(id) FROM tb_sensor WHERE esp_id = 'soil1_data' 
-            GROUP BY (SELECT SUBSTRING(created_at, 1, 10));
-
-            // Prints October 3, 1975 was on a Friday
-            echo "Oct 3,1975 was on a ".date("l", mktime(0,0,0,10,3,1975)) . "<br>";
-            2024-08-17 23:12:08
-        */ 
-        $totalJumlahSensor = IndividualBlockSensor::select(DB::raw("COUNT(id) AS jumlah"))
+    public function dataSensorSuhuPerHari() {
+        $totalJumlahSensor = IndividualBlockSensor::select(DB::raw("AVG(suhu) AS jumlah"))
             ->where("esp_id", "soil1_data")
-            ->groupBy(DB::raw("(SELECT SUBSTRING(created_at, 1, 10))"))
+            ->groupBy(DB::raw("SUBSTRING(created_at, 1, 10)"))
             ->limit("7")
             ->get();
 
         $hariPerTotalJumlahSensor = IndividualBlockSensor::select(DB::raw("created_at"))
             ->where("esp_id", "soil1_data")
-            ->groupBy(DB::raw("created_at"))
+            ->groupBy(DB::raw("SUBSTRING(created_at, 1, 10)"))
             ->limit("7")
             ->get();
 
         $jumlahID = [0, 0, 0, 0, 0, 0, 0];
         $hari = [0, 0, 0, 0, 0, 0, 0];
         for($i = 0; $i < count($totalJumlahSensor); $i++) {
-            $jumlahID[$i] = $totalJumlahSensor[$i]->jumlah;
-
             $arrTemp = explode("-", $hariPerTotalJumlahSensor[$i]->created_at);
             $dateArr = explode(" ", $arrTemp[2]);
-            $hari[$i] = date("l", mktime(0, 0, 0, $arrTemp[1], $dateArr[0], $arrTemp[0]));
+            
+            switch(date("l", mktime(0, 0, 0, $arrTemp[1], $dateArr[0], $arrTemp[0]))) {
+                case "Monday":
+                    $hari[0] = date("l", mktime(0, 0, 0, $arrTemp[1], $dateArr[0], $arrTemp[0]));
+                    $jumlahID[0] = $totalJumlahSensor[$i]->jumlah;
+                break;
+                case "Tuesday":
+                    $hari[1] = date("l", mktime(0, 0, 0, $arrTemp[1], $dateArr[0], $arrTemp[0]));
+                    $jumlahID[1] = $totalJumlahSensor[$i]->jumlah;
+                break;
+                case "Wednesday":
+                    $hari[2] = date("l", mktime(0, 0, 0, $arrTemp[1], $dateArr[0], $arrTemp[0]));
+                    $jumlahID[2] = $totalJumlahSensor[$i]->jumlah;
+                break;
+                case "Thursday":
+                    $hari[3] = date("l", mktime(0, 0, 0, $arrTemp[1], $dateArr[0], $arrTemp[0]));
+                    $jumlahID[3] = $totalJumlahSensor[$i]->jumlah;
+                break;
+                case "Friday":
+                    $hari[4] = date("l", mktime(0, 0, 0, $arrTemp[1], $dateArr[0], $arrTemp[0]));
+                    $jumlahID[4] = $totalJumlahSensor[$i]->jumlah;
+                break;
+                case "Saturday":
+                    $hari[5] = date("l", mktime(0, 0, 0, $arrTemp[1], $dateArr[0], $arrTemp[0]));
+                    $jumlahID[5] = $totalJumlahSensor[$i]->jumlah;
+                break;
+                case "Sunday":
+                    $hari[6] = date("l", mktime(0, 0, 0, $arrTemp[1], $dateArr[0], $arrTemp[0]));
+                    $jumlahID[6] = $totalJumlahSensor[$i]->jumlah;
+                break;
+            }
         }
-        return (response()->json([
+        return response()->json([
+            'hariID1' => ($hari[0] == 0) ? 0 : $hari[0],
+            'hariID2' => ($hari[1] == 0) ? 0 : $hari[1],
+            'hariID3' => ($hari[2] == 0) ? 0 : $hari[2],
+            'hariID4' => ($hari[3] == 0) ? 0 : $hari[3],
+            'hariID5' => ($hari[4] == 0) ? 0 : $hari[4],
+            'hariID6' => ($hari[5] == 0) ? 0 : $hari[5],
+            'hariID7' => ($hari[6] == 0) ? 0 : $hari[6],
+
+            'countID1' => ($jumlahID[0] == 0) ? 0 : $jumlahID[0],
+            'countID2' => ($jumlahID[1] == 0) ? 0 : $jumlahID[1],
+            'countID3' => ($jumlahID[2] == 0) ? 0 : $jumlahID[2],
+            'countID4' => ($jumlahID[3] == 0) ? 0 : $jumlahID[3],
+            'countID5' => ($jumlahID[4] == 0) ? 0 : $jumlahID[4],
+            'countID6' => ($jumlahID[5] == 0) ? 0 : $jumlahID[5],
+            'countID7' => ($jumlahID[6] == 0) ? 0 : $jumlahID[6],
+        ]);
+    }
+
+    public function dataSensorCahayaPerHari() {
+        $totalJumlahSensor = IndividualBlockSensor::select(DB::raw("AVG(cahaya) AS jumlah"))
+            ->where("esp_id", "soil2_data")
+            ->groupBy(DB::raw("SUBSTRING(created_at, 1, 10)"))
+            ->limit("7")
+            ->get();
+
+        $hariPerTotalJumlahSensor = IndividualBlockSensor::select(DB::raw("created_at"))
+            ->where("esp_id", "soil2_data")
+            ->groupBy(DB::raw("SUBSTRING(created_at, 1, 10)"))
+            ->limit("7")
+            ->get();
+
+        $jumlahID = [0, 0, 0, 0, 0, 0, 0];
+        $hari = [0, 0, 0, 0, 0, 0, 0];
+        for($i = 0; $i < count($totalJumlahSensor); $i++) {
+            $arrTemp = explode("-", $hariPerTotalJumlahSensor[$i]->created_at);
+            $dateArr = explode(" ", $arrTemp[2]);
+            
+            switch(date("l", mktime(0, 0, 0, $arrTemp[1], $dateArr[0], $arrTemp[0]))) {
+                case "Monday":
+                    $hari[0] = date("l", mktime(0, 0, 0, $arrTemp[1], $dateArr[0], $arrTemp[0]));
+                    $jumlahID[0] = $totalJumlahSensor[$i]->jumlah;
+                break;
+                case "Tuesday":
+                    $hari[1] = date("l", mktime(0, 0, 0, $arrTemp[1], $dateArr[0], $arrTemp[0]));
+                    $jumlahID[1] = $totalJumlahSensor[$i]->jumlah;
+                break;
+                case "Wednesday":
+                    $hari[2] = date("l", mktime(0, 0, 0, $arrTemp[1], $dateArr[0], $arrTemp[0]));
+                    $jumlahID[2] = $totalJumlahSensor[$i]->jumlah;
+                break;
+                case "Thursday":
+                    $hari[3] = date("l", mktime(0, 0, 0, $arrTemp[1], $dateArr[0], $arrTemp[0]));
+                    $jumlahID[3] = $totalJumlahSensor[$i]->jumlah;
+                break;
+                case "Friday":
+                    $hari[4] = date("l", mktime(0, 0, 0, $arrTemp[1], $dateArr[0], $arrTemp[0]));
+                    $jumlahID[4] = $totalJumlahSensor[$i]->jumlah;
+                break;
+                case "Saturday":
+                    $hari[5] = date("l", mktime(0, 0, 0, $arrTemp[1], $dateArr[0], $arrTemp[0]));
+                    $jumlahID[5] = $totalJumlahSensor[$i]->jumlah;
+                break;
+                case "Sunday":
+                    $hari[6] = date("l", mktime(0, 0, 0, $arrTemp[1], $dateArr[0], $arrTemp[0]));
+                    $jumlahID[6] = $totalJumlahSensor[$i]->jumlah;
+                break;
+            }
+        }
+        return response()->json([
             'hariID1' => ($hari[0] == 0) ? 0 : $hari[0],
             'hariID2' => ($hari[1] == 0) ? 0 : $hari[1],
             'hariID3' => ($hari[2] == 0) ? 0 : $hari[2],
@@ -90,7 +172,145 @@ class SensorController extends Controller {
             'countID5' => ($jumlahID[4] == 0) ? 0 : $jumlahID[4],
             'countID6' => ($jumlahID[5] == 0) ? 0 : $jumlahID[5],
             'countID7' => ($jumlahID[6] == 0) ? 0 : $jumlahID[6]
-        ]));
+        ]);
+    }
+
+    public function dataSensorUdaraPerHari() {
+        $totalJumlahSensor = IndividualBlockSensor::select(DB::raw("AVG(kelembaban) AS jumlah"))
+            ->where("esp_id", "soil1_data")
+            ->groupBy(DB::raw("SUBSTRING(created_at, 1, 10)"))
+            ->limit("7")
+            ->get();
+
+        $hariPerTotalJumlahSensor = IndividualBlockSensor::select(DB::raw("created_at"))
+            ->where("esp_id", "soil1_data")
+            ->groupBy(DB::raw("SUBSTRING(created_at, 1, 10)"))
+            ->limit("7")
+            ->get();
+
+        $jumlahID = [0, 0, 0, 0, 0, 0, 0];
+        $hari = [0, 0, 0, 0, 0, 0, 0];
+        for($i = 0; $i < count($totalJumlahSensor); $i++) {
+            $arrTemp = explode("-", $hariPerTotalJumlahSensor[$i]->created_at);
+            $dateArr = explode(" ", $arrTemp[2]);
+            
+            switch(date("l", mktime(0, 0, 0, $arrTemp[1], $dateArr[0], $arrTemp[0]))) {
+                case "Monday":
+                    $hari[0] = date("l", mktime(0, 0, 0, $arrTemp[1], $dateArr[0], $arrTemp[0]));
+                    $jumlahID[0] = $totalJumlahSensor[$i]->jumlah;
+                break;
+                case "Tuesday":
+                    $hari[1] = date("l", mktime(0, 0, 0, $arrTemp[1], $dateArr[0], $arrTemp[0]));
+                    $jumlahID[1] = $totalJumlahSensor[$i]->jumlah;
+                break;
+                case "Wednesday":
+                    $hari[2] = date("l", mktime(0, 0, 0, $arrTemp[1], $dateArr[0], $arrTemp[0]));
+                    $jumlahID[2] = $totalJumlahSensor[$i]->jumlah;
+                break;
+                case "Thursday":
+                    $hari[3] = date("l", mktime(0, 0, 0, $arrTemp[1], $dateArr[0], $arrTemp[0]));
+                    $jumlahID[3] = $totalJumlahSensor[$i]->jumlah;
+                break;
+                case "Friday":
+                    $hari[4] = date("l", mktime(0, 0, 0, $arrTemp[1], $dateArr[0], $arrTemp[0]));
+                    $jumlahID[4] = $totalJumlahSensor[$i]->jumlah;
+                break;
+                case "Saturday":
+                    $hari[5] = date("l", mktime(0, 0, 0, $arrTemp[1], $dateArr[0], $arrTemp[0]));
+                    $jumlahID[5] = $totalJumlahSensor[$i]->jumlah;
+                break;
+                case "Sunday":
+                    $hari[6] = date("l", mktime(0, 0, 0, $arrTemp[1], $dateArr[0], $arrTemp[0]));
+                    $jumlahID[6] = $totalJumlahSensor[$i]->jumlah;
+                break;
+            }
+        }
+        return response()->json([
+            'hariID1' => ($hari[0] == 0) ? 0 : $hari[0],
+            'hariID2' => ($hari[1] == 0) ? 0 : $hari[1],
+            'hariID3' => ($hari[2] == 0) ? 0 : $hari[2],
+            'hariID4' => ($hari[3] == 0) ? 0 : $hari[3],
+            'hariID5' => ($hari[4] == 0) ? 0 : $hari[4],
+            'hariID6' => ($hari[5] == 0) ? 0 : $hari[5],
+            'hariID7' => ($hari[6] == 0) ? 0 : $hari[6],
+
+            'countID1' => ($jumlahID[0] == 0) ? 0 : $jumlahID[0],
+            'countID2' => ($jumlahID[1] == 0) ? 0 : $jumlahID[1],
+            'countID3' => ($jumlahID[2] == 0) ? 0 : $jumlahID[2],
+            'countID4' => ($jumlahID[3] == 0) ? 0 : $jumlahID[3],
+            'countID5' => ($jumlahID[4] == 0) ? 0 : $jumlahID[4],
+            'countID6' => ($jumlahID[5] == 0) ? 0 : $jumlahID[5],
+            'countID7' => ($jumlahID[6] == 0) ? 0 : $jumlahID[6]
+        ]);
+    }
+
+    public function dataSensorTanahPerHari() {
+        $totalJumlahSensor = IndividualBlockSensor::select(DB::raw("AVG(kelembabantanah) AS jumlah"))
+            // ->where("esp_id", "soil1_data")
+            ->groupBy(DB::raw("SUBSTRING(created_at, 1, 10)"))
+            ->limit("7")
+            ->get();
+
+        $hariPerTotalJumlahSensor = IndividualBlockSensor::select(DB::raw("created_at"))
+            // ->where("esp_id", "soil1_data")
+            ->groupBy(DB::raw("SUBSTRING(created_at, 1, 10)"))
+            ->limit("7")
+            ->get();
+
+        $jumlahID = [0, 0, 0, 0, 0, 0, 0];
+        $hari = [0, 0, 0, 0, 0, 0, 0];
+        for($i = 0; $i < count($totalJumlahSensor); $i++) {
+            $arrTemp = explode("-", $hariPerTotalJumlahSensor[$i]->created_at);
+            $dateArr = explode(" ", $arrTemp[2]);
+            
+            switch(date("l", mktime(0, 0, 0, $arrTemp[1], $dateArr[0], $arrTemp[0]))) {
+                case "Monday":
+                    $hari[0] = date("l", mktime(0, 0, 0, $arrTemp[1], $dateArr[0], $arrTemp[0]));
+                    $jumlahID[0] = $totalJumlahSensor[$i]->jumlah;
+                break;
+                case "Tuesday":
+                    $hari[1] = date("l", mktime(0, 0, 0, $arrTemp[1], $dateArr[0], $arrTemp[0]));
+                    $jumlahID[1] = $totalJumlahSensor[$i]->jumlah;
+                break;
+                case "Wednesday":
+                    $hari[2] = date("l", mktime(0, 0, 0, $arrTemp[1], $dateArr[0], $arrTemp[0]));
+                    $jumlahID[2] = $totalJumlahSensor[$i]->jumlah;
+                break;
+                case "Thursday":
+                    $hari[3] = date("l", mktime(0, 0, 0, $arrTemp[1], $dateArr[0], $arrTemp[0]));
+                    $jumlahID[3] = $totalJumlahSensor[$i]->jumlah;
+                break;
+                case "Friday":
+                    $hari[4] = date("l", mktime(0, 0, 0, $arrTemp[1], $dateArr[0], $arrTemp[0]));
+                    $jumlahID[4] = $totalJumlahSensor[$i]->jumlah;
+                break;
+                case "Saturday":
+                    $hari[5] = date("l", mktime(0, 0, 0, $arrTemp[1], $dateArr[0], $arrTemp[0]));
+                    $jumlahID[5] = $totalJumlahSensor[$i]->jumlah;
+                break;
+                case "Sunday":
+                    $hari[6] = date("l", mktime(0, 0, 0, $arrTemp[1], $dateArr[0], $arrTemp[0]));
+                    $jumlahID[6] = $totalJumlahSensor[$i]->jumlah;
+                break;
+            }
+        }
+        return response()->json([
+            'hariID1' => ($hari[0] == 0) ? 0 : $hari[0],
+            'hariID2' => ($hari[1] == 0) ? 0 : $hari[1],
+            'hariID3' => ($hari[2] == 0) ? 0 : $hari[2],
+            'hariID4' => ($hari[3] == 0) ? 0 : $hari[3],
+            'hariID5' => ($hari[4] == 0) ? 0 : $hari[4],
+            'hariID6' => ($hari[5] == 0) ? 0 : $hari[5],
+            'hariID7' => ($hari[6] == 0) ? 0 : $hari[6],
+
+            'countID1' => ($jumlahID[0] == 0) ? 0 : $jumlahID[0],
+            'countID2' => ($jumlahID[1] == 0) ? 0 : $jumlahID[1],
+            'countID3' => ($jumlahID[2] == 0) ? 0 : $jumlahID[2],
+            'countID4' => ($jumlahID[3] == 0) ? 0 : $jumlahID[3],
+            'countID5' => ($jumlahID[4] == 0) ? 0 : $jumlahID[4],
+            'countID6' => ($jumlahID[5] == 0) ? 0 : $jumlahID[5],
+            'countID7' => ($jumlahID[6] == 0) ? 0 : $jumlahID[6]
+        ]);
     }
 
 }
