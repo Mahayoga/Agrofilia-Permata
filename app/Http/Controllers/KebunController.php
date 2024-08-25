8c6166bbcf23dd0514fca7d3d29bede6e72ade0b
Mahayoga <myoga.bahtiar@gmail.com>
2024-08-25 19:31:05 +0700

    Nambah method 'ambilDataStatusKebun'

--- app/Http/Controllers/KebunController.php
+++ app/Http/Controllers/KebunController.php
@@ -25,4 +25,37 @@ class KebunController extends Controller {
             "dataLength" => count($daftarBlok)
         ]);
     }
+
+    public function ambilDataStatusKebun($id) {
+        $dataStatus = BlokModel::select()
+            ->where("id_detail_blok", $id)
+            ->get();
+
+        $resultUmur = 0;
+        $jenisUmur = "";
+        $yearsNow = (int)date("Y");
+        $yearsInData = (int)explode("-", $dataStatus[0]->created_at)[0];
+        $resultUmur = $yearsNow - $yearsInData;
+        $jenisUmur = "Tahun";
+        if(($yearsNow - $yearsInData) == 0) {
+            $monthNow = (int)date("m");
+            $monthInData = (int)explode("-", $dataStatus[0]->created_at)[1];
+            $resultUmur = $monthNow - $monthInData;
+            $jenisUmur = "Bulan";
+            if(($monthNow - $monthInData) == 0) {
+                $dayNow = (int)date("d");
+                $dayInData = (int)explode(" ", explode("-", $dataStatus[0]->created_at)[2])[0];
+                $resultUmur = $dayNow - $dayInData;
+                $jenisUmur = "Hari";
+            }
+        }
+
+        return response()->json([
+            "jenisUmur" => $jenisUmur,
+            "umurBlok" => $resultUmur,
+            "tahunBlok" => $yearsInData,
+            "statusBlok" => $dataStatus[0]->status_blok,
+            "faseBlok" => $dataStatus[0]->fase_tanaman
+        ]);
+    }
 }
