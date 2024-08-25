2edccb014a97a3f9cc6e61058d039fe70cbbe263
Mahayoga <myoga.bahtiar@gmail.com>
2024-08-26 06:12:36 +0700

    Ubah kode di dalam bagian pupuk yang indikator kapan di update

--- resources/views/dashboard-template.blade.php
+++ resources/views/dashboard-template.blade.php
@@ -373,7 +373,12 @@
               <hr class="dark horizontal">
               <div class="d-flex ">
                 <i class="material-icons text-sm my-auto me-1">schedule</i>
-                <p class="mb-0 text-sm"> update 1 jam yang lalu </p>
+                <p class="mb-0 text-sm"> data terakhir <span id="updateTimePupuk"></span> yang lalu </p>
+                <script>
+                  $.get("{{ route('pupukLastUpdateTime') }}", function(data) {
+                    document.getElementById("updateTimePupuk").innerText = data.timeResult;
+                  });
+                </script>
               </div>
             </div>
           </div>
