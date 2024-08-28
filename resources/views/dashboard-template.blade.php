2224df446db67b35bc30a87965634622da923a44
Mahayoga <myoga.bahtiar@gmail.com>
2024-08-28 23:47:19 +0700

    Nambah fungsi bagian grafik soil biar bisa ambil data waktu terakhir update dari database

--- resources/views/dashboard-template.blade.php
+++ resources/views/dashboard-template.blade.php
@@ -628,7 +628,12 @@
               <hr class="dark horizontal">
               <div class="d-flex ">
                 <i class="material-icons text-sm my-auto me-1">schedule</i>
-                <p class="mb-0 text-sm">update 1 jam yang lalu</p>
+                <p class="mb-0 text-sm"> data terakhir <span id="updateTimeTanah"></span> yang lalu </p>
+                <script>
+                  $.get("{{ route('tanahLastUpdateTime') }}", function(data) {
+                    document.getElementById("updateTimeTanah").innerText = data.timeResult;
+                  });
+                </script>
               </div>
             </div>
           </div>
