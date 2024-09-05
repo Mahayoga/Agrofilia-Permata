c4f05b79b6d5947c279e8f17d68281d120aa2fbb
Mahayoga <myoga.bahtiar@gmail.com>
2024-09-06 00:56:21 +0700

    Oiya lupa hapus notifikasi

--- resources/views/pages/notifications.blade.php
+++ resources/views/pages/notifications.blade.php
@@ -226,61 +226,6 @@
       </div>
     </div>
     <div class="container-fluid py-4">
-      <div class="row">
-        <div class="col-lg-4 col-md-4 mx-auto">
-          <div class="card mt-4">
-            <div class="card-header p-3">
-              <h5 class="mb-0">Notifikasi</h5>
-            </div>
-            <div class="card-body p-3 pb-0">
-              {{-- 
-                alert-primary
-                alert-secondary
-                alert-success
-                alert-danger
-                alert-warning
-                alert-info
-                alert-light
-                alert-dark
-              --}}
-              <div class="alert alert-primary alert-dismissible text-white" role="alert">
-                <div class="d-flex flex-column">
-                  <div class="text-md">Peringatan</div>
-                  <div class="text-sm">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum eos, hic omnis unde nemo aspernatur velit fugiat ex, maiores porro architecto beatae. Corrupti provident porro officia molestiae inventore, voluptates harum. <a href="javascript:;" class="alert-link text-white">Cek disini</a></div>
-                  <div class="text-xs mt-2">16 Agustus 2024 21.22</div>
-                </div>
-                <button type="button" class="btn-close text-lg py-3 opacity-10" data-bs-dismiss="alert" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-            </div>
-          </div>
-          <div class="card mt-4  d-none">
-            <div class="card-header p-3">
-              <h5 class="mb-0">Notifications</h5>
-              <p class="text-sm mb-0">
-                Notifications on this page use Toasts from Bootstrap. Read more details <a href="https://getbootstrap.com/docs/5.0/components/toasts/" target="">here</a>.
-              </p>
-            </div>
-            <div class="card-body p-3">
-              <div class="row">
-                <div class="col-lg-3 col-sm-6 col-12">
-                  <button class="btn bg-gradient-success w-100 mb-0 toast-btn" id="toastBtn" type="button" data-target="successToast">Success</button>
-                </div>
-                <div class="col-lg-3 col-sm-6 col-12 mt-sm-0 mt-2">
-                  <button class="btn bg-gradient-info w-100 mb-0 toast-btn" type="button" data-target="infoToast">Info</button>
-                </div>
-                <div class="col-lg-3 col-sm-6 col-12 mt-lg-0 mt-2">
-                  <button class="btn bg-gradient-warning w-100 mb-0 toast-btn" type="button" data-target="warningToast">Warning</button>
-                </div>
-                <div class="col-lg-3 col-sm-6 col-12 mt-lg-0 mt-2">
-                  <button class="btn bg-gradient-danger w-100 mb-0 toast-btn" type="button" data-target="dangerToast">Danger</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
       <div class="position-fixed bottom-1 end-1 z-index-2">
         <div class="toast fade hide p-2 bg-white" role="alert" aria-live="assertive" id="successToast" aria-atomic="true">
           <div class="toast-header border-0">
