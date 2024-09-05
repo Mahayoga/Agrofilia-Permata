bd187980fec8060e2fe1458c18256d1a35fe9ece
Mahayoga <myoga.bahtiar@gmail.com>
2024-09-05 17:52:34 +0700

    Redirect halaman default ke login buat sementara

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login-template');
 });
 
 Route::get('/readDataRataRata', [App\Http\Controllers\SensorController::class, 'dataRataRata'])->name('readDataRataRata');
