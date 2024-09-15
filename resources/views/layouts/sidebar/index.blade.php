55ea5906328afb174db9f1e01139a659189a28b8
Mahayoga <myoga.bahtiar@gmail.com>
2024-09-15 13:51:28 +0700

    Fix sidebar: Role

--- resources/views/layouts/sidebar/index.blade.php
+++ resources/views/layouts/sidebar/index.blade.php
@@ -44,22 +44,26 @@
       <li class="nav-item mt-3">
         <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Halaman Tambah Data</h6>
       </li>
-      <li class="nav-item">
-        <a class="{{ Request::is('pengguna/create') ? 'active bg-gradient-primary' : '' }} nav-link text-white " href="{{ route('pengguna.create') }}">
-          <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-            <i class="material-icons opacity-10">folder</i>
-          </div>
-          <span class="nav-link-text ms-1">Data User</span>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="{{ Request::is('galeri*') ? 'active bg-gradient-primary' : '' }} nav-link text-white " href="{{ route('galeri.index') }}">
-          <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-            <i class="material-icons opacity-10">folder</i>
-          </div>
-          <span class="nav-link-text ms-1">Data Galeri</span>
-        </a>
-      </li>
+      @if (Auth::user()->role == "Super Admin")
+        <li class="nav-item">
+          <a class="{{ Request::is('pengguna/create') ? 'active bg-gradient-primary' : '' }} nav-link text-white " href="{{ route('pengguna.create') }}">
+            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="material-icons opacity-10">folder</i>
+            </div>
+            <span class="nav-link-text ms-1">Data User</span>
+          </a>
+        </li>
+      @endif
+      @if (Auth::user()->role == "Manager" || Auth::user()->role == "Super Admin")
+        <li class="nav-item">
+          <a class="{{ Request::is('galeri*') ? 'active bg-gradient-primary' : '' }} nav-link text-white " href="{{ route('galeri.index') }}">
+            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="material-icons opacity-10">folder</i>
+            </div>
+            <span class="nav-link-text ms-1">Data Galeri</span>
+          </a>
+        </li>
+      @endif
       <li class="nav-item mt-3">
         <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Halaman Pengguna</h6>
       </li>
