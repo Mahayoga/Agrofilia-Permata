124e90674027199fd873cbd921a2cdae0658e31d
Mahayoga <myoga.bahtiar@gmail.com>
2024-09-23 00:21:35 +0700

    Add: User tiap role

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -24,5 +24,41 @@ class UserSeeder extends Seeder
             'password' => password_hash('admin1234', CRYPT_BLOWFISH, ['cost' => 12]),
             'remember_token' => null
         ]);
+
+        User::create([
+            'name' => 'Admin Vanili',
+            'role' => 'Admin',
+            'nomor_telepon' => '082-337-341-446',
+            'alamat' => 'Mastrip V',
+            'deskripsi' => 'Saya adalah admin dari kebun vanili',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => null,
+            'password' => password_hash('admin1234', CRYPT_BLOWFISH, ['cost' => 12]),
+            'remember_token' => null
+        ]);
+
+        User::create([
+            'name' => 'Manager Vanili',
+            'role' => 'Manager',
+            'nomor_telepon' => '083-473-665-221',
+            'alamat' => 'Mastrip IV',
+            'deskripsi' => 'Saya adalah manager dari kebun vanili',
+            'email' => 'manager@gmail.com',
+            'email_verified_at' => null,
+            'password' => password_hash('admin1234', CRYPT_BLOWFISH, ['cost' => 12]),
+            'remember_token' => null
+        ]);
+
+        User::create([
+            'name' => 'Pengurus Kebun Vanili',
+            'role' => 'Pengurus Kebun',
+            'nomor_telepon' => '083-473-665-221',
+            'alamat' => 'Batu Raden',
+            'deskripsi' => 'Saya adalah pengurus kebun vanili',
+            'email' => 'pengurus@gmail.com',
+            'email_verified_at' => null,
+            'password' => password_hash('admin1234', CRYPT_BLOWFISH, ['cost' => 12]),
+            'remember_token' => null
+        ]);
     }
 }
